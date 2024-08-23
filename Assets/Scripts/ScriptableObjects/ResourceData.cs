dd0b5cdc10ad00301f53f08ab70dad970a4c3c48
Apo100u <mailodspamu@o2.pl>
2024-08-24 00:54:59 +0200

    develop -- resource data SO class

---
+++ Assets/Scripts/ScriptableObjects/ResourceData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace SurvivalGame.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "New Resource Data", menuName = "Survival Game/Resource Data")]
+    public class ResourceData : ScriptableObject
+    {
+        [Tooltip("GameObject of the resource that appears in the game world.")]
+        [SerializeField] private GameObject prefab;
+        [Tooltip("Image of the resource shown in UI.")]
+        [SerializeField] private Sprite image;
+
+        public int GetId()
+        {
+            return GetHashCode();
+        }
+    }
+}
