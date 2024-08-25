aa1fec5fb2b0da0e232810d2f40752af3d238989
Apo100u <mailodspamu@o2.pl>
2024-08-26 01:05:12 +0200

    develop -- add item components and colliders to items

--- Assets/Scripts/Gameplay/Entities/Components/InteractionHandler.cs
+++ Assets/Scripts/Gameplay/Entities/Components/InteractionHandler.cs
@@ -1,12 +1,35 @@
 using UnityEngine;
+using SurvivalGame.Gameplay.Helpers;
 
 namespace SurvivalGame.Gameplay.Entities.Components
 {
     public class InteractionHandler : MonoBehaviour
     {
         [Header("Collider to search for interactables.")]
-        [SerializeField] private Collider interactionsArea;
+        [SerializeField] private TriggerCallbackDispatcher triggerCallbackDispatcher;
         
+        
+
+        private void OnEnable()
+        {
+            triggerCallbackDispatcher.TriggerEntered += OnInteractionTriggerEntered;
+            triggerCallbackDispatcher.TriggerExited += OnInteractionTriggerExited;
+        }
+
+        private void OnDisable()
+        {
+            triggerCallbackDispatcher.TriggerEntered -= OnInteractionTriggerEntered;
+            triggerCallbackDispatcher.TriggerExited -= OnInteractionTriggerExited;
+        }
+
+        private void OnInteractionTriggerEntered(Collider other)
+        {
+        }
+
+        private void OnInteractionTriggerExited(Collider other)
+        {
+        }
+
         public void TryInteractWithClosestInteractable()
         {
             
