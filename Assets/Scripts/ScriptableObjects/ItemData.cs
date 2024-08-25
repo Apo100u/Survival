d4bf52da2fbfad3a248025b94d716a4a8a286974
Apo100u <mailodspamu@o2.pl>
2024-08-25 21:12:32 +0200

    refactor -- rename ResourceData to ItemData

--- Assets/Scripts/ScriptableObjects/ItemData.cs
+++ Assets/Scripts/ScriptableObjects/ItemData.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 namespace SurvivalGame.ScriptableObjects
 {
-    [CreateAssetMenu(fileName = "New Resource Data", menuName = "Survival Game/Resource Data")]
-    public class ResourceData : ScriptableObject
+    [CreateAssetMenu(fileName = "New Item Data", menuName = "Survival Game/Item Data")]
+    public class ItemData : ScriptableObject
     {
+        [Tooltip("Human-friendly name for the resource. This is how the resource will be called in the game.")]
+        [SerializeField] private string displayName;
+        
         [Tooltip("GameObject of the resource that appears in the game world.")]
         [SerializeField] private GameObject prefab;
         
--- Assets/Scripts/ScriptableObjects/RecipeData.cs
+++ Assets/Scripts/ScriptableObjects/RecipeData.cs
@@ -5,11 +5,11 @@ namespace SurvivalGame.ScriptableObjects
     [CreateAssetMenu(fileName = "New Recipe Data", menuName = "Survival Game/Recipe Data")]
     public class RecipeData : ScriptableObject
     {
-        [Tooltip("Resources needed to for this recipe in any order.")]
-        [SerializeField] private ResourceData[] ingredients;
+        [Tooltip("Items needed to for this recipe in any order.")]
+        [SerializeField] private ItemData[] ingredients;
         
-        [Tooltip("Resource received when crafting is successful.")]
-        [SerializeField] private ResourceData successfulOutput;
+        [Tooltip("Item received when crafting is successful.")]
+        [SerializeField] private ItemData successfulOutput;
 
         [Tooltip("Chance of receiving successful output. Range 0 - 1 (0 is 0%, 1 is 100%).")]
         [SerializeField, Range(0f, 1f)] private float successChance = 0.5f;
