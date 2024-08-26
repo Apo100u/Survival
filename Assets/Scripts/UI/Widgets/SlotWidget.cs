17600a243eca4e0391f0716b80ecf8bc210594f1
Apo100u <mailodspamu@o2.pl>
2024-08-26 22:09:12 +0200

    develop -- highlight slot widget on mouse hover

--- Assets/Scripts/UI/Widgets/SlotWidget.cs
+++ Assets/Scripts/UI/Widgets/SlotWidget.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace SurvivalGame.UI.Widgets
 {
-    public class SlotWidget : Widget
+    public class SlotWidget : Widget, IPointerEnterHandler, IPointerExitHandler
     {
+        [Header("Dependencies")]
+        [SerializeField] private Image background;
         [SerializeField] private Image displayImage;
 
+        [Header("Dependencies")]
+        [SerializeField] private Color defaultBackgroundColor = Color.white;
+        [SerializeField] private Color highlightedBackgroundColor  = Color.white;
+        
         public void SetDisplayImage(Sprite sprite)
         {
             displayImage.gameObject.SetActive(sprite);
@@ -15,6 +22,16 @@ namespace SurvivalGame.UI.Widgets
             {
                 displayImage.sprite = sprite;
             }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            background.color = highlightedBackgroundColor;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            background.color = defaultBackgroundColor;
         }
     }
 }
